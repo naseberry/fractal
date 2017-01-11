6cb86de26ac66053a48a8df27809bb385ee8fd76
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-01-11 12:59:09 +0000

    Add initial fixture files

---
+++ test/fixtures/components/button/button.config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    context: {
+        buttonText: 'A button',
+        class: 'Button'
+    },
+    variants: [{
+        name: 'red',
+        context: {
+            buttonText: 'A red button',
+            class: 'Button Button--red'
+        }
+    }]
+};
